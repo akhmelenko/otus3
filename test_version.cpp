0184523352005ed82b11ed404886b97211cd3f3f
akhmelenko <30999411+akhmelenko@users.noreply.github.com>
2024-01-05 16:33:17 +0300

    version range check

--- test_version.cpp
+++ test_version.cpp
@@ -15,8 +15,8 @@ BOOST_AUTO_TEST_SUITE(test_version)
 BOOST_AUTO_TEST_CASE(test_valid_version)
 // #pragma GCC diagnostic pop 
 {
-    std::cout << "version=" << version() << std::endl;
     BOOST_CHECK(version() > 6);
+    BOOST_CHECK(version() < 16);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
