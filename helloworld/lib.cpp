8a17230f0f007071dc2b128d52886ca5d67c34c9
akhmelenko <30999411+akhmelenko@users.noreply.github.com>
2024-01-05 16:20:44 +0300

    Check current version on github

--- helloworld/lib.cpp
+++ helloworld/lib.cpp
@@ -2,6 +2,9 @@
 
 #include "version.h"
 
+#include <iostream>
+
 int version() {
+    std::cout << "PROJECT_VERSION_PATCH: " << PROJECT_VERSION_PATCH << std::endl;
     return PROJECT_VERSION_PATCH;
 }
