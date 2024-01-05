cd2ea6acb470aa93fd67833c7eb8f3a5bb154ab3
akhmelenko <30999411+akhmelenko@users.noreply.github.com>
2024-01-05 15:40:20 +0300

    Update test_version.cpp
    Comment pragma

--- test_version.cpp
+++ test_version.cpp
@@ -5,13 +5,13 @@
 #include <boost/test/unit_test.hpp>
 // #include <gtest/gtest.h>
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wc99-extensions"
+// #pragma GCC diagnostic push
+// #pragma GCC diagnostic ignored "-Wc99-extensions"
 
 BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
-#pragma GCC diagnostic pop 
+// #pragma GCC diagnostic pop 
 {
     BOOST_CHECK(version() < 100);
 }
