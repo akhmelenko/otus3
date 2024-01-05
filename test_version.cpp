c4b7aab806cdf6e320f11b8101b1af7d9c56be25
Anton Khmelenko <aakhmelenko@sberbank.ru>
2024-01-05 15:21:51 +0300

    My lesson1 add local increment version

--- test_version.cpp
+++ test_version.cpp
@@ -1,14 +1,19 @@
 #define BOOST_TEST_MODULE test_version
 
-#include "lib.h"
+#include "helloworld/lib.h"
 
 #include <boost/test/unit_test.hpp>
+// #include <gtest/gtest.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wc99-extensions"
 
 BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
+#pragma GCC diagnostic pop 
 {
-    BOOST_CHECK(version() != 100);
+    BOOST_CHECK(version() < 100);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
