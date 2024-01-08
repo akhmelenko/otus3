d7c463f05f0f4cbaa8df4addb65f13c7217a4d8c
akhmelenko <30999411+akhmelenko@users.noreply.github.com>
2024-01-08 23:59:07 +0300

    Update

--- test_version.cpp
+++ test_version.cpp
@@ -2,7 +2,7 @@
 #include "db/db.h"
 #include "version/version.h"
 
-#include <gmock/gmock.h>
+// #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 TEST(TestGroupVersion, TestBuildVersion) {
@@ -17,43 +17,43 @@ TEST(TestGroupVersion, Subtest_2) {
   ASSERT_FALSE('a' == 'b');
 }
 
-class MockConnection : public Connection {
-    public:
-    MOCK_METHOD(
-        bool,
-        Login,
-        (std::string& username, std::string& password),
-        (override)
-    );
+// class MockConnection : public Connection {
+//     public:
+//     MOCK_METHOD(
+//         bool,
+//         Login,
+//         (std::string& username, std::string& password),
+//         (override)
+//     );
 
-    MOCK_METHOD(
-        bool,
-        Logout,
-        (),
-        (override)
-    );
-};
+//     MOCK_METHOD(
+//         bool,
+//         Logout,
+//         (),
+//         (override)
+//     );
+// };
 
-TEST(TestGroupConnection, MockTest) {
-    std::string nic = "my_nic";
-    std::string pass = "my_pass";
-    MockConnection mock_connection;
+// TEST(TestGroupConnection, MockTest) {
+//     std::string nic = "my_nic";
+//     std::string pass = "my_pass";
+//     MockConnection mock_connection;
 
-    // Prepare mocks
-    EXPECT_CALL(mock_connection, Login(testing::_, testing::_))
-        .Times(testing::AtLeast(1))
-        .WillOnce(testing::Return(true));
+//     // Prepare mocks
+//     EXPECT_CALL(mock_connection, Login(testing::_, testing::_))
+//         .Times(testing::AtLeast(1))
+//         .WillOnce(testing::Return(true));
 
-    EXPECT_CALL(mock_connection, Logout())
-        .Times(testing::AtLeast(1));
+//     EXPECT_CALL(mock_connection, Logout())
+//         .Times(testing::AtLeast(1));
 
-    DataBase db(&mock_connection, nic, pass);
-}
+//     DataBase db(&mock_connection, nic, pass);
+// }
 
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
-  ::testing::InitGoogleMock(&argc, argv);
+  // ::testing::InitGoogleMock(&argc, argv);
   
   return RUN_ALL_TESTS();
 }
