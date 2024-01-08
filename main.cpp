2269ea4251fb4eec020e305bac61548271d1f46b
Anton Khmelenko <aakhmelenko@sberbank.ru>
2024-01-08 23:16:43 +0300

    Comment gmock

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
-#include "helloworld/lib.h"
+#include "version/version.h"
+#include "db/dummy_connection.h"
+#include "db/db.h"
 
 #include <iostream>
 
 int main (int, char **) {
     std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, worldw!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    DummyConnection dc;
+    std::string nic = "my_nic";
+    std::string pass = "my_pass";
+    DataBase db(&dc, nic, pass);
     return 0;
 }
